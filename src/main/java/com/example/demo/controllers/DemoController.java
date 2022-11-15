62397e906fcb97af80103dcec7d3c8035971f0b3
andrukhmykyta <117035035+andrukhmykyta@users.noreply.github.com>
2022-11-15 19:40:44 +0200

    Update DemoController.java

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -8,16 +8,11 @@ public class DemoController {
 
     @GetMapping("/")
     public String first() {
-        return "Hello from Spring Boot!";
+        return "петро мотіватор!";
     }
     
-    @GetMapping("/lab")
+    @GetMapping("/petro")
     public String second() {
-        return "Lab 6 was successfully done!";
-    }
-    
-    @GetMapping("/x")
-    public String secondx() {
-        return "x!";
+        return "Я ЖИВИИИИИИИИИИИИИИИИИИИИИИИИИИЙ!";
     }
 }
